f4cb5547ab9839f61c0d40eb45cab176e9f3a229
fitshahaan <shahaansyed@fitternity.com>
2017-06-01 20:47:05 +0530

    Done

--- CustomerNormalize/src/com/fitternity/jobs/UnsubscribeCustomers.java
+++ CustomerNormalize/src/com/fitternity/jobs/UnsubscribeCustomers.java
@@ -54,32 +54,34 @@ public class UnsubscribeCustomers extends TimerTask {
 		BulkWriteOperation bulkWriteOperation = custDao.getBulkWriteOp();
 		try 
 		{
+			System.out.println("HI "+new File(System.getProperty("user.dir")+"/input/Unsubscribe Customers/Unsubsrcibe List - Sheet1.csv").getName());
 			BufferedReader br=new BufferedReader(new FileReader(new File(System.getProperty("user.dir")+"/input/Unsubscribe Customers/Unsubsrcibe List - Sheet1.csv")));
 			String line="";
 			HashSet<String> emails=new HashSet<>();
 			int counter=0;
-			System.out.println(" TOTAL LINES "+br.lines());
-			/*while((line=br.readLine())!=null)
+//			System.out.println(" TOTAL LINES "+br.lines());
+			while((line=br.readLine())!=null)
 			{
 				String currentEmail=line.split(",")[0];
-				if(currentEmail!=null&&"".equalsIgnoreCase(currentEmail))
+//				System.out.println(" currentEmail " +currentEmail);
+				if(currentEmail!=null&&!"".equalsIgnoreCase(currentEmail))
 				{
+					System.out.println(currentEmail);
 					emails.add(currentEmail);	
 					counter++;
+					System.out.println(" currentEmail :: "+currentEmail +" _ counter :: "+counter);
 				}
 				if(counter==5000)
 				{
 					updateInDB(emails,bulkWriteOperation);
-					emails.clear();
 					counter=0;
 				}
 			}
 			if (counter<5000&&counter>0) 
 			{
 				updateInDB(emails,bulkWriteOperation);
-				emails.clear();
-				counter=0;
-			}*/
+//				counter=0;
+			}
 		} 
 		catch (IOException e) 
 		{
@@ -91,9 +93,18 @@ public class UnsubscribeCustomers extends TimerTask {
 	
 	private void updateInDB(HashSet<String> emails,BulkWriteOperation bulkWriteOperation)
 	{
-			bulkWriteOperation.find(new BasicDBObject("email", new BasicDBObject("$in",emails.toArray(new String[emails.size()])))).update(new BasicDBObject("$set",new BasicDBObject("unsubscribed customer", true)));
+		bulkWriteOperation = custDao.getBulkWriteOp();
+			System.out.println(emails.size());
+			bulkWriteOperation.find(new BasicDBObject("email", new BasicDBObject("$in",emails.toArray(new String[emails.size()])))).update(new BasicDBObject("$set",new BasicDBObject("unsubscribed_customer", true)));
+			/*try {
+				Thread.sleep(12000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
 			System.out.println(" 5000 DONE." + bulkWriteOperation.execute());
-			bulkWriteOperation = custDao.getBulkWriteOp();
+			emails.clear();
+		
 	}
 	
 		
