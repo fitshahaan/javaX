a78dd396694189c944810d75e6cfaf69b99d8f84
fitshahaan <shahaansyed@fitternity.com>
2017-06-07 12:51:13 +0530

    Done

---
+++ CustomerNormalize/src/com/fitternity/constants/FitConstants.java
@@ -0,0 +1,12 @@
+package com.fitternity.constants;
+/**
+ * @author Shahaan
+ *
+ */
+public interface FitConstants 
+{
+	
+	String CUSTOMER_JOB="customerJob";
+	String TRANSACTION_JOB="transactionJob";
+		
+}
